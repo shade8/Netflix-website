006b3c87a6afc11eabbd3ddc894ce7529935f004
teja <48879182+shade8@users.noreply.github.com>
2023-06-27 18:41:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    this.classList.add('tab-border');
+    const tabContentItems = document.querySelector(`#${this.id}-content`);
+    tabContentItems.classList.add('show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+tabItems.forEach(item => item.addEventListener('click', selectItem))
